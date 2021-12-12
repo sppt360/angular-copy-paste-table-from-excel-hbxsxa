dd36e2710b0805f2c9d3a733fc79d49e1f443b11
sppt360 <sppulithevan@gmail.com>
2021-12-12 15:32:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-copy-paste-table-from-excel-5fsa7a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-copy-paste-table-from-excel-5fsa7a)
---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Paste it";
+  val: any;
+  displayedColumns: string[];
+  dataSource: any[] = [];
+  firstColumnDropdown: any[] = [];
+
+  data(event: ClipboardEvent) {
+    let clipboardData = event.clipboardData;
+    let pastedText = clipboardData.getData("text");
+    let row_data = pastedText.split("\n");
+    this.displayedColumns = row_data[0].split("\t");
+    delete row_data[0];
+    // Create table dataSource
+    let data = [];
+
+    row_data.forEach(row_data => {
+      let row = {};
+      this.displayedColumns.forEach((a, index) => {
+        row[a] = row_data.split("\t")[index];
+      });
+      this.firstColumnDropdown.push(row[this.displayedColumns[0]]);
+      data.push(row);
+    });
+    console.log(data);
+    this.dataSource = data;
+  }
+}
